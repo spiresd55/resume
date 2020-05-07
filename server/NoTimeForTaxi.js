76e48e1ca0344788789b650036ee6bd05af6fe24
David Spires <david.spires@aurea.com>
2020-05-07 15:05:29 -0500

    creates coordinate plane based on starting and end point

--- server/NoTimeForTaxi.js
+++ server/NoTimeForTaxi.js
@@ -182,6 +182,54 @@ class BinaryHeap {
     }
 }
 
+class Vertex {
+    constructor(key) {
+        this.key = key;
+        this.edges = {};
+    }
+
+    addEdge(edge, weight) {
+        this.edges[edge.key] = {edge, weight};
+    }
+
+    getWeight(edge) {
+        return this.edges[edge].weight;
+    }
+
+    getConnections() {
+        return this.edges;
+    }
+}
+
+class Graph {
+    constructor() {
+        this.vertices = {}
+    }
+
+    addVertex(key) {
+        this.vertices[key] = new Vertex(key);
+    }
+
+    getVertice(key) {
+        return this.vertices[key] || undefined;
+    }
+
+    contains(key) {
+        return this.vertices[key] ? true: false;
+    }
+
+    addEdge(fromKey, toKey, weight){
+        if (!this.vertices[fromKey]) {
+            this.addVertex(fromKey);
+        }
+        if (!this.vertices[toKey]){
+            this.addVertex(toKey)
+        }
+
+        this.vertices[fromKey].addEdge(this.vertices[toKey], weight)
+    }
+
+}
 
 const isValidDirection = (direction) => direction === "R" || direction === "L";
 
@@ -218,13 +266,93 @@ const calculateOriginalPath = (startingPosition, instructions) => {
         }
     });
 
-    return path;
+    //Set Boundaries (will use this later to create a coordinate plane graph
+    let minX = startingPosition.x,
+        maxX = startingPosition.x,
+        minY = startingPosition.y,
+        maxY = startingPosition.y;
+
+    //Determine boundaries
+
+    path.forEach((p) => {
+        minX = Math.min(minX, p.x);
+        maxX = Math.max(maxX, p.x);
+        minY = Math.min(minY, p.y);
+        maxY = Math.max(maxY, p.y)
+    });
+
+    return {path, minX, maxX, minY, maxY};
+};
+
+const createCoordinatePlane = (pathDetails) => {
+    const {path, minX, maxX, minY, maxY} = pathDetails;
+    console.log('coordinate plane', path[0], path[path.length - 1]);
+
+    //Create Empty Graph
+    const coordinatePlane = new Graph();
+
+    //Draw Coordinate Plane
+    for(let i = minX; i <= maxX; i++) {
+        for(let j = minY; j <= maxY; j++) {
+            console.log(i, j);
+
+            const currentPoint = `(${i},${j})`;
+            //Corners
+            if (i === minX && j === minY) {
+                //Bottom Left Corner
+                coordinatePlane.addEdge(currentPoint, `(${i + 1},${j})`, 1);
+                coordinatePlane.addEdge(currentPoint, `(${i},${j + 1})`, 1);
+                console.log(coordinatePlane.getVertice(`(${i},${j})`))
+            } else if (i === minX && j === maxY) {
+                //Top Left Corner
+                coordinatePlane.addEdge(currentPoint, `(${i + 1},${j})`,1);
+                coordinatePlane.addEdge(currentPoint,  `(${i - 1},${j})`,1)
+            } else if (i === maxX && j === minY) {
+                //Bottom right corner
+                coordinatePlane.addEdge(currentPoint,`(${i-1},${j})`,1);
+                coordinatePlane.addEdge(currentPoint,`(${i},${j+1})`,1);
+            } else if (i === maxX && j === maxY) {
+                //Top right corner
+                coordinatePlane.addEdge(currentPoint,`(${i-1},${j})`,1);
+                coordinatePlane.addEdge(currentPoint,`(${i},${j-1})`,1);
+            } else if (j === minY) {
+                //Bottom Edge
+                coordinatePlane.addEdge(currentPoint,`(${i-1},${j})`, 1);
+                coordinatePlane.addEdge(currentPoint,`(${i+1},${j})`,1);
+                coordinatePlane.addEdge(currentPoint,`(${i},${j+1})`,1);
+            } else if (j === maxY) {
+                //Top Edge
+                coordinatePlane.addEdge(currentPoint,`(${i-1},${j})`,1);
+                coordinatePlane.addEdge(currentPoint,`(${i+1},${j})`,1);
+                coordinatePlane.addEdge(currentPoint,`(${i},${j-1})`,1);
+            } else if (i === minX) {
+                //Left Edge
+                coordinatePlane.addEdge(currentPoint,`(${i},${j+1})`,1);
+                coordinatePlane.addEdge(currentPoint,`(${i},${j-1})`,1);
+                coordinatePlane.addEdge(currentPoint,`(${i+1},${j})`,1);
+            } else if (i === maxX) {
+                //Right Edge
+                coordinatePlane.addEdge(currentPoint,`(${i},${j+1})`,1);
+                coordinatePlane.addEdge(currentPoint,`(${i},${j-1})`,1);
+                coordinatePlane.addEdge(currentPoint,`(${i-1},${j})`,1);
+            } else {
+                //Other
+                coordinatePlane.addEdge(currentPoint,`(${i+1},${j})`,1);
+                coordinatePlane.addEdge(currentPoint,`(${i-1},${j})`,1);
+                coordinatePlane.addEdge(currentPoint,`(${i},${j-1})`,1);
+                coordinatePlane.addEdge(currentPoint,`(${i},${j+1})`,1);
+            }
+        }
+    }
+
+    return coordinatePlane;
 };
 
 
 
 const results = calculateOriginalPath(new Coordinate(3,5), ["R3", "L3", "L3", "R4"]);
 console.log(results);
+createCoordinatePlane(results);
 
 
 //console.log("TESTING HEAP");
